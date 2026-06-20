6795dda7bf9d2cbe36ccb0dde69c8855bd86874b
Oliver <oliver.ferri77@gmail.com>
2026-06-20 15:40:07 -0500

    readme and images

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -10,33 +10,21 @@ to search for general data anaylst, change salary average from hour to year
 and remove job_title condition and change to job_tite_short = 'Data Analyst'
 */
 
-WITH top_jobs AS (
-    SELECT
-        DENSE_RANK() OVER (ORDER BY salary_hour_avg DESC) AS rnk,
-        job_id,
-        job_title,
-        job_location,
-        job_schedule_type,
-        salary_hour_avg,
-        job_title_short,
-        company_id,
-        CAST(job_posted_date AS DATE) AS job_posted_date
-    FROM job_postings_fact
-    WHERE 
-        job_title LIKE '%tableau%'
-        AND salary_hour_avg IS NOT NULL
-        AND job_location = 'Anywhere'
-)
-
-SELECT 
+SELECT
+    DENSE_RANK() OVER (ORDER BY salary_hour_avg DESC) AS rnk,
     job_id,
-    job_title,
-    job_title_short,
     name AS company_name,
+    job_title,
     job_location,
     job_schedule_type,
     salary_hour_avg,
-    job_posted_date
-FROM top_jobs j
-JOIN company_dim c ON j.company_id = c.company_id
-WHERE rnk <= 12;
+    job_title_short,
+    f.company_id,
+    CAST(job_posted_date AS DATE) AS job_posted_date
+FROM job_postings_fact f
+JOIN company_dim c ON f.company_id = c.company_id
+WHERE 
+    job_title LIKE '%tableau%'
+    AND salary_hour_avg IS NOT NULL
+    AND job_location = 'Anywhere'
+LIMIT 12;
